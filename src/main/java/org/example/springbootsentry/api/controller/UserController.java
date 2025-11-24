75f078abb969a4c64a4de20aec306f842d80dece
anhvh1732002 <88437305+ViktorHoanh@users.noreply.github.com>
2025-11-24 22:53:36 +0700

    Add Sentry

--- src/main/java/org/example/springbootsentry/api/controller/UserController.java
+++ src/main/java/org/example/springbootsentry/api/controller/UserController.java
@@ -2,6 +2,8 @@ package org.example.springbootsentry.api.controller;
 
 import org.example.springbootsentry.api.model.User;
 import org.example.springbootsentry.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,6 +14,7 @@ import java.util.Optional;
 @RestController
 public class UserController {
 
+    private static final Logger log = LoggerFactory.getLogger(UserController.class);
     private UserService userService;
 
     @Autowired
@@ -21,10 +24,17 @@ public class UserController {
 
     @GetMapping("/user")
     public User getUser(@RequestParam int id) {
+        log.info("Get User with id {}", id);
         Optional user = userService.getUser(id);
         if (user.isPresent()) {
             return (User) user.get();
         }
         return null;
+    }
+
+    @GetMapping("/errors")
+    public String triggerException(){
+        log.info("Trigger Exception");
+        throw new RuntimeException("error roài hẹ hẹ hẹ");
     }
 }
